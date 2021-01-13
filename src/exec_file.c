d59e8b8d92a1e09aa6ff05d091913bba4d86bb72
franciscoreinolds <francisco.reinolds@outlook.pt>
2021-01-13 16:27:09 +0000

    Descoberto o bug dos pipes

---
+++ src/exec_file.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int main (int argc, char* argv[]) {
+    // ../bin/aurrasd-filters/aurrasd-echo < sample-1.m4a > output.mp3
+
+    char* trans1 = "/bin/wc";
+    char* og_file = "./1pipe.c";
+    char* dest_file = "wc_output.txt";
+
+    // file -> stdin
+    int open_f = open(og_file, O_RDONLY);
+    printf("open_f: %d\n", open_f);
+    dup2(open_f,0);
+    close(open_f);
+
+    // stdout -> file
+    int fd = open(dest_file, O_WRONLY | O_APPEND | O_CREAT, 0644);
+    printf("fd: %d\n", fd);
+    dup2(fd,1);
+    close(fd);
+
+    execl(trans1, trans1, NULL);
+    exit(1);
+}
